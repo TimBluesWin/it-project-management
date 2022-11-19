7753d4852f72b3894677c82318f355a5db100396
Timothy Aditya Sutantyo <timisutantyo@gmail.com>
2022-11-19 17:23:31 +0100

    Prepare for connection with React. Many websites suggest using Corsheaders, so I use that

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders' # this will be installed later
 ]
 
 MIDDLEWARE = [
@@ -47,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
 ]
 
 ROOT_URLCONF = 'django_project.urls'
@@ -125,3 +127,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# this should be put at the end of the settings.py file
+CORS_ORIGIN_WHITELIST = [
+     'http://localhost:3000'
+]
