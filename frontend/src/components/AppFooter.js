15cfb398941a02a6f9113a69057b07714ca590c8
Timothy Aditya Sutantyo <timisutantyo@gmail.com>
2023-01-06 11:32:22 +0100

    Fix footer

--- frontend/src/components/AppFooter.js
+++ frontend/src/components/AppFooter.js
@@ -5,10 +5,14 @@ const AppFooter = () => {
   return (
     <CFooter>
       <div>
-        <a href="https://coreui.io" target="_blank" rel="noopener noreferrer">
-          CoreUI
+        <a
+          href="https://www.hti-handel.de/unternehmen/hti-oesterreich-gmbh"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          HTI
         </a>
-        <span className="ms-1">&copy; 2022 creativeLabs.</span>
+        <span className="ms-1">&copy; 2023.</span>
       </div>
       <div className="ms-auto">
         <span className="me-1">Powered by</span>
